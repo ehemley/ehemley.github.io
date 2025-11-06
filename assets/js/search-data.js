e57c5c72eadb25f5679b044e80a3bbc397c0b244
ehemley <128187395+ehemley@users.noreply.github.com>
2025-11-06 04:03:33 +0000

    Deploying to gh-pages from @ ehemley/ehemley.github.io@16f0416a0b5937ea12fcdf9345756342655beee3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,12 +48,7 @@ ninja.data = [{
             window.location.href = "/blog/2025/my-first-post/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
